0932f44f471475f6df60424b452e39ecfcc96fc7
Andriikryl <andreyk557@gmail.com>
2023-10-22 13:36:45 +0300

    add visuallyHidden component

--- src/components/generator/Generator.tsx
+++ src/components/generator/Generator.tsx
@@ -8,7 +8,17 @@ export default function Generator() {
         <div className={style.generator__content}>
           <div className={style.inner__box}>
             <h1 className={style.generator__title}>Glassmorphism</h1>
-            <div className={style.glassmorf__box}></div>
+            <div
+              style={{
+                background: "rgba(255, 255, 255, 0.35)",
+                borderRadius: "16px",
+                boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+                backdropFilter: "blur(10.1px)",
+                WebkitBackdropFilter: "blur(10.1px)",
+                border: "1px solid rgba(255, 255, 255, 0.32)",
+              }}
+              className={style.glassmorf__box}
+            ></div>
           </div>
         </div>
       </Container>
